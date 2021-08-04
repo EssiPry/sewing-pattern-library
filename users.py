b2af30c22dce422c689109733045a01d59d4ba26
Essi Prykäri <essi.prykari@helsinki.fi>
2021-08-04 20:48:44 +0300

    add check-hash-value to login

--- users.py
+++ users.py
@@ -1,16 +1,21 @@
 from db import db 
 from flask import session
 from werkzeug.security import check_password_hash, generate_password_hash
-import os 
+from os import getenv
+
 
 def login(username, password):
-    sql = "SELECT id, password FROM users WHERE username=:username"
+    sql = "SELECT id, username, password FROM users WHERE username=:username"
     result = db.session.execute(sql, {"username":username})
     user = result.fetchone()
     if not user:
         return False
     else: 
-        return True 
+        if check_password_hash(user.password, password):
+            session["user_id"] = user.id
+            session["user_name"] = user.username
+            return True 
+        return False
 
 def register(username, password): 
     hash_value = generate_password_hash(password)
