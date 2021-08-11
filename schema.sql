6e789d7b936309fdb7b740dffcf5ce70ab55d72d
Essi Prykäri <essi.prykari@helsinki.fi>
2021-08-12 00:06:01 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,2 +1,4 @@
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT);  
 CREATE TABLE patterns (id SERIAL PRIMARY KEY, name TEXT UNIQUE, company TEXT, fabric INTEGER); 
+CREATE TABLE reviews (id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users, pattern_id INTEGER REFERENCES patterns, review TEXT, date DATE); 
+
