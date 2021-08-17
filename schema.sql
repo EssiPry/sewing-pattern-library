af2d68104fc5669a953845841aa3590388dca6c6
Essi Prykäri <essi.prykari@helsinki.fi>
2021-08-17 18:23:15 +0300

    Update db fabric type to text in  table patterns

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT);  
-CREATE TABLE patterns (id SERIAL PRIMARY KEY, name TEXT UNIQUE, company TEXT, fabric INTEGER); 
+CREATE TABLE patterns (id SERIAL PRIMARY KEY, name TEXT UNIQUE, company TEXT, fabric TEXT); 
 CREATE TABLE reviews (id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users, pattern_id INTEGER REFERENCES patterns, review TEXT, date DATE); 
 CREATE TABLE garments (id SERIAL PRIMARY KEY, garment TEXT); 
 CREATE TABLE garments_in_pattern (id SERIAL PRIMARY KEY, pattern_id INTEGER REFERENCES patterns, garment_id INTEGER REFERENCES garments); 
