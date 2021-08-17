43a324e10c46ac8f99ab5b45905d52e62ad0178e
Essi Prykäri <essi.prykari@helsinki.fi>
2021-08-17 18:25:35 +0300

    Update adding pattern to db to reflect change in table patterns

--- sewingpatterns.py
+++ sewingpatterns.py
@@ -17,27 +17,32 @@ def add_garment_type_to_pattern(pattern_id, garment_id):
     db.session.execute(sql, {"pattern_id":pattern_id, "garment_id":garment_id})
     db.session.commit()
 
-def get_patterns_by_name(name):
+def get_patterns(name, company, fabric): 
     name = name 
-    sql = "SELECT * FROM patterns WHERE name LIKE :name"
-    return db.session.execute(sql, {"name":"%"+name+"%"}).fetchall()
+    company = company
+    fabric=fabric
+    sql = "SELECT P.name, P.company, P.fabric FROM patterns P WHERE P.name LIKE :name AND P.company LIKE :company AND P.fabric LIKE :fabric" 
+    return db.session.execute(sql, {"name": "%"+name+"%","company":"%"+company+"%", "fabric": fabric})  
 
 def get_pattern_by_name(name):
     name = name
     sql = "SELECT * FROM patterns WHERE name = :name"
     return db.session.execute(sql, {"name":name}).fetchone()
 
-def count_by_name(name):
-    name = name
-    sql ="SELECT COUNT(*) FROM patterns WHERE name LIKE :name"
-    return db.session.execute(sql, {"name":"%"+name+"%"}).fetchone()[0]
+def count_patterns(name, company, fabric):
+    sql ="SELECT COUNT(*) FROM patterns WHERE name LIKE :name AND company LIKE :company AND fabric LIKE :fabric"
+    return db.session.execute(sql, {"name":"%"+name+"%", "company":"%"+company+"%", "fabric":"%"+fabric+"%"}).fetchone()[0]
 
 def get_pattern_id(name):
-    name = name
+    name = name 
     sql ="SELECT id FROM patterns WHERE name = :name"
     return db.session.execute(sql, {"name":name}).fetchone()[0]
 
 def get_garment_id(garment):
-    garment=garment
+    garment = garment
     sql ="SELECT id FROM garments WHERE garment = :garment"
     return db.session.execute(sql, {"garment":garment}).fetchone()[0]
+
+def get_all_garments(): 
+    sql = "SELECT * FROM garments ORDER BY id"
+    return db.session.execute(sql).fetchall()
