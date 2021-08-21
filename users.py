ef9bda3b05b2b5f139430b2065539cb2654a5c6b
Essi Prykäri <essi.prykari@helsinki.fi>
2021-08-21 13:51:27 +0300

    Lint users.py

--- users.py
+++ users.py
@@ -1,8 +1,7 @@
-from db import db 
+from os import getenv
+from db import db
 from flask import session
 from werkzeug.security import check_password_hash, generate_password_hash
-from os import getenv
-
 
 def login(username, password):
     sql = "SELECT id, username, password FROM users WHERE username=:username"
@@ -10,21 +9,19 @@ def login(username, password):
     user = result.fetchone()
     if not user:
         return False
-    else: 
-        if check_password_hash(user.password, password):
-            session["user_id"] = user.id
-            session["user_name"] = user.username
-            return True 
-        return False
+    if check_password_hash(user.password, password):
+        session["user_id"] = user.id
+        session["user_name"] = user.username
+        return True
+    return False
 
 def logout():
     del session["user_id"]
     del session["user_name"]
 
-
-def register(username, password): 
+def register(username, password):
     hash_value = generate_password_hash(password)
-    try: 
+    try:
         sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
         db.session.execute(sql, {"username":username, "password":hash_value})
         db.session.commit()
