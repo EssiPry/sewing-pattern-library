ebf6bee90d8946c6a8db15a22da401701e4e2de1
Essi Prykäri <essi.prykari@helsinki.fi>
2021-08-22 14:19:36 +0300

    Add deleting pattern from personal pattern library

--- my_patterns.py
+++ my_patterns.py
@@ -8,3 +8,22 @@ def add_to_my_patterns(user_id, pattern_id):
         return True
     except:
         return False
+
+def delete_from_my_patterns(user_id, pattern_id):
+    try:
+        sql = "DELETE FROM my_patterns WHERE user_id=:user_id AND pattern_id=:pattern_id"
+        db.session.execute(sql, {"user_id":user_id, "pattern_id":pattern_id})
+        db.session.commit()
+        return True
+    except:
+        return False
+
+def get_my_patterns(user_id):
+    sql = """SELECT P.company, P.name
+             FROM patterns P, my_patterns A
+             WHERE A.pattern_id = P.id AND A.user_id = :user_id"""
+    return db.session.execute(sql, {"user_id":user_id}).fetchall()
+
+def in_db(user_id, pattern_id):
+    sql = "SELECT * FROM my_patterns WHERE user_id=:user_id AND pattern_id=:pattern_id"
+    return db.session.execute(sql, {"user_id":user_id, "pattern_id":pattern_id}).fetchone()
--- routes.py
+++ routes.py
@@ -79,28 +79,41 @@ def pattern_page(pattern_name):
     sewing_pattern = sewingpatterns.get_pattern_by_name(pattern_name)
     garments = sewingpatterns.get_garments(pattern_name)
     pattern_reviews = reviews.get_reviews(pattern_name)
+    user_id = users.get_user_id()
+    pattern_id = sewingpatterns.get_pattern_id(pattern_name)
+    in_db = my_patterns.in_db(user_id, pattern_id)
     if request.method == "POST":
-        user_id = users.get_user_id()
         pattern_id = sewingpatterns.get_pattern_id(pattern_name)
         review = request.form["review"]
         if reviews.add_review(user_id, pattern_id, review):
             pattern_reviews = reviews.get_reviews(pattern_name)
-            return render_template("pattern.html", pattern_name=sewing_pattern.name, company=sewing_pattern.company, fabric=sewing_pattern.fabric, garments=garments, reviews=pattern_reviews)
+            return render_template("pattern.html", pattern_name=sewing_pattern.name, company=sewing_pattern.company, fabric=sewing_pattern.fabric, garments=garments, reviews=pattern_reviews, in_db=in_db)
         return render_template("error.html", message="Something went wrong, please try again")
-    return render_template("pattern.html", pattern_name=sewing_pattern.name, company=sewing_pattern.company, fabric=sewing_pattern.fabric, garments=garments, reviews=pattern_reviews)
+    return render_template("pattern.html", pattern_name=sewing_pattern.name, company=sewing_pattern.company, fabric=sewing_pattern.fabric, garments=garments, reviews=pattern_reviews, in_db=in_db)
+
+@app.route("/my_patternlibrary")
+def my_patternlibrary():
+    user_id=users.get_user_id()
+    my_sewingpatterns = my_patterns.get_my_patterns(user_id)
+    return render_template("my_patternlibrary.html", my_patterns=my_sewingpatterns)
 
 @app.route("/add_to_my_patterns", methods=["POST"])
 def add_to_my_patterns():
     pattern_name=request.form["pattern_name"]
+    print(pattern_name)
     pattern_id = sewingpatterns.get_pattern_id(pattern_name)
+    print(pattern_id)
     user_id = users.get_user_id()
-    my_patterns.add_to_my_patterns(user_id, pattern_id)
-    return redirect("/")
+    if my_patterns.add_to_my_patterns(user_id, pattern_id):
+        return redirect("/my_patternlibrary")
+    return render_template("error.html", message="Something went wrong, please try again")
 
 @app.route("/delete_from_my_patterns", methods=["POST"])
-def add_to_my_patterns():
+def delete_from_my_patterns():
     pattern_name=request.form["pattern_name"]
     pattern_id = sewingpatterns.get_pattern_id(pattern_name)
     user_id = users.get_user_id()
-    my_patterns.add_to_my_patterns(user_id, pattern_id)
-    return redirect("/")
+    print(user_id, pattern_id)
+    if my_patterns.delete_from_my_patterns(user_id, pattern_id):
+        return redirect("/")
+    return render_template("error.html", message="Something went wrong, please try again")
