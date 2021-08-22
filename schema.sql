92816493da3b86dafb153adb894c605a8b5ef141
Essi Prykäri <essi.prykari@helsinki.fi>
2021-08-22 14:17:11 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
-CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT);  
-CREATE TABLE patterns (id SERIAL PRIMARY KEY, name TEXT UNIQUE, company TEXT, fabric TEXT); 
-CREATE TABLE reviews (id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users, pattern_id INTEGER REFERENCES patterns, review TEXT, date DATE); 
-CREATE TABLE garments (id SERIAL PRIMARY KEY, garment TEXT); 
-CREATE TABLE garments_in_pattern (id SERIAL PRIMARY KEY, pattern_id INTEGER REFERENCES patterns, garment_id INTEGER REFERENCES garments); 
-
+CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT);
+CREATE TABLE patterns (id SERIAL PRIMARY KEY, name TEXT UNIQUE, company TEXT, fabric TEXT);
+CREATE TABLE reviews (id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users, pattern_id INTEGER REFERENCES patterns, review TEXT, date DATE);
+CREATE TABLE garments (id SERIAL PRIMARY KEY, garment TEXT);
+CREATE TABLE garments_in_pattern (id SERIAL PRIMARY KEY, pattern_id INTEGER REFERENCES patterns, garment_id INTEGER REFERENCES garments);
+CREATE TABLE my_patterns (id SERIAL PRIMARY KEY,  user_id INTEGER REFERENCES users, pattern_id INTEGER REFERENCES patterns)
